b176439a5c9aeb0b35929a42a453e8c14cd46901
Angel Alicea <ala31996@gmail.com>
2019-12-03 15:50:49 -0500

    set up

--- server.js
+++ server.js
@@ -3,3 +3,26 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const movies = require('./movieData')
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+// app.use(function validateBearerToken(req, res, next){
+//   const apiToken = process.env.API_TOKEN
+//   // const authToken = 
+
+
+// });
+
+
+console.log(process.env.API_TOKEN)
+
+app.get('/movie', function handleGetMovie(req,res){
+    res.json(movies)
+})
+app.listen(8000, () => {
+    console.log('Server started on PORT 8000');
+  });
