a527475e26c39620e0f20fac0c4fb3d22c6d5789
Angel Alicea <ala31996@gmail.com>
2019-12-03 16:51:07 -0500

    completed movie drill

--- server.js
+++ server.js
@@ -3,26 +3,50 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const movies = require('./movieData');
+const movies = require('./movieData.json');
 const app = express();
 
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 
-// app.use(function validateBearerToken(req, res, next){
-//   const apiToken = process.env.API_TOKEN
-//   // const authToken = 
+app.use(function validateBearerToken(req, res, next){
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
 
-
-// });
-
-
-console.log(process.env.API_TOKEN);
+	if(!authToken || authToken.split(' ')[1] !== apiToken){
+		return res.status(401).json({error: 'unathorized request'})
+	}
+	next()
+});
 
 app.get('/movie', function handleGetMovie(req,res){
-  res.json(movies);
+  let results = movies;
+	if(req.query.genre){
+		results= results.filter(movie =>
+			movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())	
+		)
+	}
+  if(req.query.country){
+		results = results.filter(movie => 
+			movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+		)
+	}
+	if(req.query.avg_vote){
+        results = results.filter(movie => 
+            Number(movie.avg_vote) >= Number(req.query.avg_vote)
+        )
+	}
+    res.json(results);
+
+
 });
+
+
+
+
+
+
 app.listen(8000, () => {
   console.log('Server started on PORT 8000');
 });
